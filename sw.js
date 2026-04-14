ab0a5f208e36123e264fe56e93cba6a0a6f34f7d
qazpoiwsx <vivoy33sba@gmail.com>
2026-04-14 13:01:04 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,25 @@
+const CACHE_NAME = 'ai-agronomist-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+  // Добавьте другие критичные файлы, если они есть
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/script.js',
-        '/icons/icon-192x192.png'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .catch(err => console.log('Cache addAll error:', err))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => fetch(event.request))
   );
 });
