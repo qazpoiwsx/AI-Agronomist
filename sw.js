576387ece2f55d2e6747a885f861293c660c20ea
qazpoiwsx <vivoy33sba@gmail.com>
2026-04-14 12:53:56 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
 self.addEventListener('fetch', () => {});
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/icons/icon-192x192.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
